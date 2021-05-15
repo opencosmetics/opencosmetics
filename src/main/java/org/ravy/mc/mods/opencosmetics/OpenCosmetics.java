08bdc10bb20ff2243b70f97da3ff389ab1935193
Ravy <ravy@farfrom.earth>
2021-05-15 02:14:13 +0200

    add sample model regisration code

--- src/main/java/org/ravy/mc/mods/opencosmetics/OpenCosmetics.java
+++ src/main/java/org/ravy/mc/mods/opencosmetics/OpenCosmetics.java
@@ -1,8 +1,12 @@
 package org.ravy.mc.mods.opencosmetics;
 
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import org.ravy.mc.mods.opencosmetics.models.HeadDummyModel;
+import org.ravy.mc.mods.opencosmetics.renderers.HeadRenderer;
 
 @Mod(modid = org.ravy.mc.mods.opencosmetics.OpenCosmetics.MODID, version = org.ravy.mc.mods.opencosmetics.OpenCosmetics.VERSION)
 public class OpenCosmetics
@@ -13,6 +17,6 @@ public class OpenCosmetics
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-
+        // MinecraftForge.EVENT_BUS.register(new HeadRenderer(new ResourceLocation("opencosmetics", "textures/red.png"), new HeadDummyModel()));
     }
 }
